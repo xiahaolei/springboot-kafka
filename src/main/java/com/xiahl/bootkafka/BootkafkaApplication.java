e8ef21137314ae5d012aaa2ba242fdf7b55d3d52
xiahl <1169555812@qq.com>
2022-03-14 14:46:07 +0800

    springboot 集成kafka

---
+++ src/main/java/com/xiahl/bootkafka/BootkafkaApplication.java
@@ -0,0 +1,13 @@
+package com.xiahl.bootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootkafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootkafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiahl/bootkafka/BootkafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiahl.bootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootkafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
